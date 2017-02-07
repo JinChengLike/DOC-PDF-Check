505279a519ec28ba96dea314860715e75b6729e4
jincheng <cjin0820@126.com>
2017-02-07 16:51:07 +0800

    add sql

---
+++ app/db_model.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+import MySQLdb
+import time
+
+
+def db_insert(id, name, result, test):
+    conn = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        passwd="jackjin820",
+        db="test",
+        charset='utf8'
+    )
+    cursor = conn.cursor()
+    datatime = time.strftime('%Y-%m-%d-%T', time.localtime(time.time()))
+    id = str(id)
+    name = name
+    result = result
+    test = test
+    sql = "INSERT INTO text_check (id, name, result, datatime, test) VALUES (" + id + ", '" + name + "', '" + result + "', '" + datatime + "', '" + test + "');"
+    try:
+        cursor.execute(sql)
+        conn.commit()
+    except Exception as e:
+        print e
+        conn.rollback()
+    cursor.close()
+    conn.close()
+
+#if __name__ == '__main__':
+ #   db_insert(1, "jin", "大家啊说法","jin.doc")
