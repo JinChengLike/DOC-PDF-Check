37a89211ac61c4134f6ee14c89b9af5d750dd49b
jincheng <cjin0820@126.com>
2017-02-07 16:48:18 +0800

    add sql

--- app/model.py
+++ app/model.py
@@ -1,10 +1,14 @@
+#coding=utf-8
 from flask_wtf import FlaskForm
-from wtforms import SubmitField,FileField
+from wtforms import SubmitField,FileField,StringField
 from flask_wtf.file import FileField, FileRequired
+from wtforms.validators import DataRequired
 
 
 class FileCheck(FlaskForm):
     model_file = FileField(validators=[FileRequired('no file')], render_kw={'class': 'sub-btn'})
     check_file = FileField(validators=[FileRequired('no file')], render_kw={'class': 'sub-btn'})
+    name = StringField("请输入审核人姓名", validators=[DataRequired()])
+    id = StringField("请输入审核人工号", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from app import model
 from flask_uploads import UploadSet, DOCUMENTS,UploadConfiguration
 from app import PdfHandle
 from app import DocHadnle
+from app import db_model
 
 files_get = UploadSet('files', DOCUMENTS)
 UploadConfiguration(app, files_get)
@@ -17,14 +18,17 @@ def index():
     if form.validate_on_submit():
         mo_f = form.model_file.data
         che_f = form.check_file.data
+        name = form.name.data
+        id = form.id.data
         if ".doc" or ".docx" in mo_f.filename:
             mo_f.save(os.path.join(app.instance_path, 'flies', 'modelfile.doc'))
             if ".doc" or ".docx" in che_f.filename:
                 che_f.save(os.path.join(app.instance_path, 'flies', 'testfile.doc'))
                 mo_name = DocHadnle.readDocx('instance/flies/modelfile.doc')
                 tes_name = DocHadnle.readDocx('instance/flies/testfile.doc')
-                aaa = DocHadnle.CheckDoc(mo_name,tes_name)
-                return render_template('index.html', form=form, res=aaa)
+                res = DocHadnle.CheckDoc(mo_name, tes_name)
+                db_model.db_insert(id, name, res, che_f.filename)
+                return render_template('index.html', form=form, res=res)
             else:
                 res = "请上传相同格式文件"
                 return render_template('index.html', form=form, res=res)
@@ -34,13 +38,14 @@ def index():
                 che_f.save(os.path.join(app.instance_path, 'flies', 'testfile.pdf'))
                 mo_name = PdfHandle.handle_pdf('instance/flies/modelfile.pdf')
                 tes_name = PdfHandle.handle_pdf('instance/flies/testfile.pdf')
-                aaa = PdfHandle.Check_PDF(mo_name, tes_name)
-                return render_template('index.html', form=form, res=aaa)
+                res = PdfHandle.Check_PDF(mo_name, tes_name)
+                db_model.db_insert(id, name, res, che_f.filename)
+                return render_template('index.html', form=form, res=res)
             else:
-                res= "请上传相同格式文件"
+                res = "请上传相同格式文件"
                 return render_template('index.html', form=form, res=res)
         else:
-            res= "请上传Doc文件或PDF文件"
+            res = "请上传Doc文件或PDF文件"
             return render_template('index.html', form=form, res=res)
     else:
         res = "检查结果再此显示"
